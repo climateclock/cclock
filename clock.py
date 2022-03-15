638b6af38e015847ccf76c7dd6f1397507d87233
Ka-Ping Yee <zestyping@gmail.com>
2022-03-14 21:58:24 -0700

    Add kairon-10, a hand-designed bitmap font by Ping.

--- clock.py
+++ clock.py
@@ -15,13 +15,13 @@ def run(frame):
     cv = frame.pack(*data.config.display.deadline.primary)
     carbon_module = data.module_dict['carbon_deadline_1']
 
-    symbol_label = frame.new_label('CO₂ H₂O CH₄  é å Î ç Ç', 'cclock-10', cv)
+    symbol_label = frame.new_label('CO₂ H₂O CH₄  é å Î ç Ç', 'kairon-10', cv)
     f = 0
     while True:
         text = ccui.format_deadline_module(carbon_module)
-        carbon_label = frame.new_label(text, 'cclock-10', cv)
+        carbon_label = frame.new_label(text, 'kairon-10', cv)
         f += 1
-        count_label = frame.new_label('%08d' % f, 'cclock-10', cv)
+        count_label = frame.new_label('%08d' % f, 'kairon-10', cv)
         frame.paste(0, 1, carbon_label)
         frame.paste(0, 11, symbol_label)
         frame.paste(0, 21, count_label)
