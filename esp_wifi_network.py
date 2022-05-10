9f05f4294125d61e13695de7f97c51a179351d5a
Ka-Ping Yee <zestyping@gmail.com>
2022-05-10 00:50:46 -0700

    Add Network interface and esp_wifi_network/unix_network implementations.

---
+++ esp_wifi_network.py
@@ -0,0 +1,121 @@
+from adafruit_esp32spi import adafruit_esp32spi
+import board
+import busio
+import cctime
+import gpio
+from network import Network, State
+
+
+def to_bytes(bytes_or_string):
+    if isinstance(bytes_or_string, bytes):
+        return bytes_or_string
+    return bytes(str(bytes_or_string), 'ascii')
+
+
+class EspWifi(adafruit_esp32spi.ESP_SPIcontrol):
+    """Patched version of ESP_SPIcontrol that resets without sleeping."""
+    reset_started_ms = 0
+
+    def reset(self):
+        if self._debug:
+            print('Resetting ESP32.')
+        if self._gpio0:
+            self._gpio0.direction = Direction.OUTPUT
+            self._gpio0.value = True  # not bootload mode
+        self._cs.value = True
+        self._reset.value = False
+        cctime.sleep(0.01)  # reset
+        self._reset.value = True
+        if self._gpio0:
+            self._gpio0.direction = Direction.INPUT
+        self.reset_started = cctime.monotonic()
+
+    def is_ready(self):
+        if self.reset_started_ms:
+            if cctime.monotonic() < self.reset_started_ms + 0.75:
+                return False
+            self.reset_started_ms = None
+        return not self._ready.value
+
+    def deinit(self):
+        self._cs.deinit()
+        self._ready.deinit()
+        self._reset.deinit()
+
+
+class EspWifiNetwork(Network):
+    def __init__(self, debug=False):
+        self.spi = None
+        self.esp = None
+        self.socket = None
+        self.debug = debug
+        self.wifi_retry_deadline_ms = None
+        self.set_state(State.OFFLINE)
+
+    def set_state(self, new_state):
+        self.state = new_state
+        if self.debug:
+            print(f'Network is now {self.state}.')
+
+    def enable_step(self, ssid, password):
+        if not self.esp:
+            esp32_cs = gpio.Output(board.ESP_CS)
+            esp32_ready = gpio.Output(board.ESP_BUSY)
+            esp32_reset = gpio.Output(board.ESP_RESET)
+            self.spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+            self.esp = EspWifi(self.spi, esp32_cs, esp32_ready, esp32_reset)
+            self.esp._debug = self.debug
+            self.esp.reset()
+
+        elif self.esp.status == 3:
+            self.set_state(State.ONLINE)
+
+        elif self.esp.is_ready():
+            if self.wifi_retry_deadline:
+                if cctime.monotonic() > self.wifi_retry_deadline:
+                    self.esp.reset()
+                    self.wifi_retry_deadline_ms = None
+                    return
+
+            self.esp.wifi_set_passphrase(to_bytes(ssid), to_bytes(password))
+            if not self.wifi_retry_deadline_ms:
+                self.wifi_retry_deadline = cctime.monotonic() + 30
+
+    def connect_step(self, hostname, port=None, ssl=True):
+        if not self.socket:
+            self.socket = self.esp.get_socket()
+            mode = self.esp.TLS_MODE if ssl else self.esp.TCP_MODE
+            port = port or (443 if ssl else 80)
+            self.esp.socket_open(self.socket, hostname, port, mode)
+
+        if self.esp.socket_connected(self.socket):
+            self.set_state(State.CONNECTED)
+
+    def send_step(self, data):
+        if self.esp.socket_connected(self.socket):
+            self.esp.socket_write(self.socket, data)
+        else:
+            self.set_state(State.ONLINE)
+
+    def receive_step(self):
+        if self.esp.socket_connected(self.socket):
+            available = self.esp.socket_available(self.socket)
+            if available:
+                return self.esp.socket_read(self.socket, available)
+            else:
+                return b''
+        else:
+            self.set_state(State.ONLINE)
+
+    def close_step(self):
+        self.esp.socket_close(self.socket)
+        self.set_state(State.ONLINE)
+
+    def disable_step(self):
+        if self.esp:
+            self.esp.deinit()
+            self.esp = None
+        if self.spi:
+            self.spi.deinit()
+            self.spi = None
+        self.set_state(State.OFFLINE)
---
+++ network.py
@@ -0,0 +1,50 @@
+class State:
+    OFFLINE = 'OFFLINE'  # networking is disabled
+    ONLINE = 'ONLINE'  # connected to the Internet, but not to any host
+    CONNECTED = 'CONNECTED'  # connected to a host, ready to send or receive
+
+
+class Network:
+    """Non-blocking API for network operations.  Each *_step method should
+    aim to finish within 10 ms.  If the operation is incomplete, the state
+    will be unchanged and the client can call the same method again.
+    """
+
+    # Implementations should shadow this with an instance variable.
+    state = State.OFFLINE
+
+    def __init__(self):
+        """Sets the initial state to OFFLINE."""
+        raise NotImplementedError('Network is an abstract interface')
+
+    def enable_step(self, ssid, password):
+        """In state OFFLINE, connects to the Internet, resulting in state
+        OFFLINE (call again) or ONLINE."""
+        raise NotImplementedError
+
+    def connect_step(self, hostname, ssl=True):
+        """In state ONLINE, establishes a TCP or SSL connection, resulting in
+        state ONLINE (call again) or CONNECTED (ready to send or receive)."""
+        raise NotImplementedError
+
+    def send_step(self, data):
+        """In state CONNECTED, writes data (as bytes) to the current
+        connection, resulting in state CONNECTED (ready to send or receive
+        data) or ONLINE (other side has closed the connection)."""
+        raise NotImplementedError
+
+    def receive_step(self):
+        """In state CONNECTED, reads and returns data (as bytes) from the
+        current connection, resulting in state CONNECTED (ready to send or
+        receive more data) or ONLINE or OFFLINE."""
+        raise NotImplementedError
+
+    def close_step(self):
+        """In state CONNECTED, closes the connected socket, resulting in
+        state ONLINE."""
+        raise NotImplementedError
+
+    def disable_step(self):
+        """In any state, shuts down and releases all networking resources,
+        resulting in state OFFLINE."""
+        raise NotImplementedError
