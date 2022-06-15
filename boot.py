745e5a2e6da746334daf790ef254d994cac1563f
Ka-Ping Yee <zestyping@gmail.com>
2022-06-15 12:25:15 -0700

    Set the indicator pixel colour to show whether the flash is writable over USB.

--- boot.py
+++ boot.py
@@ -1,5 +1,6 @@
 import board
 import gpio
+import neopixel
 import storage
 
 up = gpio.Button(board.BUTTON_UP)
@@ -7,7 +8,12 @@ down = gpio.Button(board.BUTTON_DOWN)
 
 # By default, the filesystem is writable from Python to let the software update
 # itself.  Hold either button on boot to make the filesystem writable over USB.
-storage.remount('/', readonly=up.pressed or down.pressed)
+usb_write_enabled = up.pressed or down.pressed
+storage.remount('/', readonly=usb_write_enabled)
+neopixel.NeoPixel(board.NEOPIXEL, 1).fill(
+    (9, 9, 9) if usb_write_enabled  # WhITE means you can WrITE
+    else (6, 0, 24)  # PuRple means PRoduction mode
+)
 
 up.deinit()
 down.deinit()
