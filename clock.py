ea28dc7c60064d63a115588e3c3ff5bc6ab4fe78
Ka-Ping Yee <zestyping@gmail.com>
2022-07-10 18:07:10 -0700

    Factor out modes into separate classes.

--- clock.py
+++ clock.py
@@ -22,78 +22,117 @@ menu = [
 ]
 
 
-class Clock:
-    def __init__(self, fs, network, data, frame, button_map, dial_map):
-        debug.mem('Clock.__init__')
+class Cycle:
+    def __init__(self, *items):
+        self.items = items
+        self.index = 0
 
+    def current(self):
+        return self.items[self.index]
+
+    def next(self):
+        self.index = (self.index + 1) % len(self.items)
+        return self.items[self.index]
+
+
+class App:
+    def __init__(self, fs, network, defn, frame, button_map, dial_map):
+        debug.mem('Clock.__init__')
+        self.network = network
+        self.frame = frame
+
+        self.clock_mode = ClockMode(self, fs, network, defn, button_map)
+        self.menu_mode = MenuMode(self, button_map, dial_map)
+        self.password_mode = PasswordMode(self, button_map, dial_map)
+        self.mode = self.clock_mode
+
+        self.langs = Cycle('en', 'es', 'de', 'fr', 'is')
+        self.lang = self.langs.current()
+
+        self.brightness_reader = DialReader('BRIGHTNESS', dial_map['BRIGHTNESS'], 3/32.0)
+        debug.mem('Clock.__init__ done')
+
+    def start(self):
+        self.frame.set_brightness(self.brightness_reader.value)
+        self.mode.start()
+
+    def step(self):
+        debug.mem('step')
+        self.brightness_reader.step(self.receive)
+        self.mode.step()
+
+    def receive(self, command, arg=None):
+        print(f'[{command}{"" if arg is None else ": " + str(arg)}]')
+        if command == 'BRIGHTNESS':
+            delta, value = arg
+            self.frame.set_brightness(value)
+        if command == 'NEXT_LANGUAGE':
+            self.lang = self.langs.next()
+            self.frame.clear()
+        if command == 'CLOCK_MODE':
+            self.set_mode(self.clock_mode)
+        if command == 'MENU_MODE':
+            self.set_mode(self.menu_mode)
+        if command == 'PASSWORD_MODE':
+            self.set_mode(self.password_mode)
+        self.mode.receive(command, arg)
+
+    def set_mode(self, mode):
+        self.mode.end()
+        self.mode = mode
+        mode.start()
+
+
+class Mode:
+    def __init__(self, app):
+        self.app = app
+        self.frame = app.frame
+
+    def start(self):
+        self.frame.clear()
+
+    def step(self):
+        pass
+
+    def receive(self, command, arg=None):
+        pass
+
+    def end(self):
+        pass
+
+
+class ClockMode(Mode):
+    def __init__(self, app, fs, network, defn, button_map):
+        super().__init__(app)
         self.fs = fs
         self.network = network
         self.fetcher = None
-        self.data = data
-        self.frame = frame
-        self.langs = ['en', 'es', 'de', 'fr', 'is']
-        self.lang_index = 0
-        self.state = 'CLOCK'
-        self.state_steps = {
-            'CLOCK': self.clock_step,
-            'MENU': self.menu_step,
-            'PASSWORD': self.password_step,
-        }
-        self.clock_reader = ButtonReader({
+        self.set_defn(defn)
+
+        self.reader = ButtonReader({
             button_map['UP']: {
                 Press.SHORT: 'NEXT_LANGUAGE',
-                Press.LONG: 'TOGGLE_CASE',
+                Press.LONG: 'TOGGLE_CAPS',
             },
             button_map['DOWN']: {
                 Press.SHORT: 'NEXT_LIFELINE',
-                Press.LONG: 'MENU',
+                Press.LONG: 'MENU_MODE',
             },
             button_map['ENTER']: {
                 Press.SHORT: 'NEXT_LIFELINE',
-                Press.SHORT: 'MENU',
+                Press.SHORT: 'MENU_MODE',
             }
         })
-        self.menu_reader = ButtonReader({
-            button_map['UP']: {
-                Press.SHORT: 'NEXT_OPTION',
-            },
-            button_map['DOWN']: {
-                Press.SHORT: 'PASSWORD',
-            },
-            button_map['ENTER']: {
-                Press.SHORT: 'PASSWORD',
-            }
-        })
-        self.password_reader = ButtonReader({
-            button_map['UP']: {
-                Press.SHORT: 'NEXT_CHAR',
-                Press.REPEAT: 'NEXT_CHAR',
-            },
-            button_map['DOWN']: {
-                Press.SHORT: 'ENTER_CHAR',
-                Press.LONG: 'CLOCK',
-            },
-            button_map['ENTER']: {
-                Press.SHORT: 'ENTER_CHAR',
-                Press.LONG: 'CLOCK',
-            }
-        })
-        self.brightness_reader = DialReader('BRIGHTNESS', dial_map['BRIGHTNESS'], 3/32.0)
-        self.frame.set_brightness(self.brightness_reader.value)
-        self.menu_selector_reader = DialReader('MENU_SELECTOR', dial_map['SELECTOR'], 1)
-        self.password_selector_reader = DialReader('PASSWORD_SELECTOR', dial_map['SELECTOR'], 1)
+        self.force_caps = False
 
-        self.carbon_module = self.data.module_dict['carbon_deadline_1']
-        self.lifeline_modules = [
-            m for m in self.data.modules if m.flavor == 'lifeline']
-        self.lifeline_index = 0
+    def set_defn(self, defn):
+        self.carbon_module = defn.module_dict['carbon_deadline_1']
+        self.lifeline_modules = Cycle(*[m for m in defn.modules if m.flavor == 'lifeline'])
+        self.lifeline_module = self.lifeline_modules.current()
+        self.deadline_cv = self.frame.pack(*defn.config.display.deadline.primary)
+        self.lifeline_cv = self.frame.pack(*defn.config.display.lifeline.primary)
 
-        self.deadline_cv = self.frame.pack(*self.data.config.display.deadline.primary)
-        self.lifeline_cv = self.frame.pack(*self.data.config.display.lifeline.primary)
-        self.menu_cv = self.frame.pack(0x80, 0x80, 0x80)
-        self.edit_cv = self.frame.pack(0x00, 0xff, 0x00)
-        self.force_upper = False
-
+    def start(self):
         self.start_time = cctime.get_time()
         self.pack_fetch_interval = 30 * 60  # wait 30 minutes between fetches
         self.next_fetch_time = self.start_time + 5
@@ -101,81 +140,118 @@ class Clock:
         self.next_advance_time = self.start_time + self.auto_advance_interval
         self.frame_count = 0
 
-        debug.mem('Clock.__init__ done')
+        self.reader.reset()
+        self.frame.clear()
 
     def step(self):
-        debug.mem('step')
-        self.brightness_reader.step(self.receive)
-        self.state_steps[self.state]()
-        self.frame_count += 1
+        ccui.render_deadline_module(
+            self.frame, 0, self.carbon_module,
+            self.deadline_cv, self.app.lang, self.force_caps)
+        ccui.render_lifeline_module(
+            self.frame, 16, self.lifeline_module,
+            self.lifeline_cv, self.app.lang, self.force_caps)
+        self.reader.step(self.app.receive)
+        self.frame.send()
+
+        self.ota_step()
 
         now = cctime.get_time()
         if now > self.next_advance_time:
+            self.next_advance_time += self.auto_advance_interval
             elapsed = now - self.start_time
             print(f'frames: {self.frame_count} / elapsed: {elapsed:.1f} / fps: {self.frame_count/elapsed:.1f}')
-            self.incr_lifeline(1)
-            self.next_advance_time += self.auto_advance_interval
+            self.lifeline_module = self.lifeline_modules.next()
+            self.frame.clear()
 
-    def clock_start(self):
-        self.frame.clear()
-        self.state = 'CLOCK'
-        self.clock_reader.reset()
-
-    def clock_step(self):
-        lang = self.langs[self.lang_index]
-        ccui.render_deadline_module(
-            self.frame, 0, self.carbon_module,
-            self.deadline_cv, lang, self.force_upper)
-        self.clock_reader.step(self.receive)
-        ccui.render_lifeline_module(
-            self.frame, 16, self.lifeline_modules[self.lifeline_index],
-            self.lifeline_cv, lang, self.force_upper)
-        self.clock_reader.step(self.receive)
-        self.frame.send()
-
+    def ota_step(self):
         if self.network.state == State.OFFLINE:
             self.network.enable_step('climateclock', 'climateclock')
             self.fetcher = None
         elif self.network.state == State.ONLINE:
             self.network.connect_step('example.com')
             self.fetcher = None
-        elif self.network.state == State.CONNECTED:
+        if self.network.state == State.CONNECTED:
             if not self.fetcher and cctime.get_time() > self.next_fetch_time:
+                # TODO: instantiate PackFetcher just once
                 self.fetcher = pack_fetcher.PackFetcher(
                     self.fs, self.network, b'example.com', b'/cclock.pk')
                 self.next_fetch_time += self.pack_fetch_interval
             if self.fetcher:
                 try:
-                    result = self.fetcher.next_step()
+                    self.fetcher.next_step()
                 except StopIteration:
-                    print('PackFetcher completed successfully')
+                    print('Fetch completed successfully')
                     self.fetcher = None
                 except Exception as e:
-                    print(f'PackFetcher aborted due to {e} ({repr(e)})')
+                    print(f'Fetcht aborted due to {e} ({repr(e)})')
                     self.fetcher = None
 
-    def menu_start(self):
-        self.frame.clear()
-        label = self.frame.new_label('Brightness', 'kairon-10')
-        self.frame.paste(1, 0, label, cv=self.menu_cv)
-        label = self.frame.new_label('Wi-Fi connection', 'kairon-10')
-        self.frame.paste(1, 11, label, cv=self.menu_cv)
-        label = self.frame.new_label('Lifelines', 'kairon-10')
-        self.frame.paste(1, 22, label, cv=self.menu_cv)
-        self.state = 'MENU'
-        self.menu_reader.reset()
+    def receive(self, command, arg=None):
+        if command == 'TOGGLE_CAPS':
+            self.force_caps = not self.force_caps
+            self.frame.clear()
+        if command == 'NEXT_LIFELINE':
+            self.lifeline_module = self.lifeline_modules.next()
+            self.frame.clear()
 
-    def menu_step(self):
-        self.menu_selector_reader.step(self.receive)
-        self.menu_reader.step(self.receive)
+
+class MenuMode(Mode):
+    def __init__(self, app, button_map, dial_map):
+        super().__init__(app)
+        self.reader = ButtonReader({
+            button_map['UP']: {
+                Press.SHORT: 'NEXT_OPTION',
+            },
+            button_map['DOWN']: {
+                Press.SHORT: 'PASSWORD_MODE',
+            },
+            button_map['ENTER']: {
+                Press.SHORT: 'PASSWORD_MODE',
+            }
+        })
+        self.dial_reader = DialReader('MENU_SELECTOR', dial_map['SELECTOR'], 1)
+        self.cv = self.frame.pack(0x80, 0x80, 0x80)
+
+    def start(self):
+        self.reader.reset()
+        self.frame.clear()
+
+        label = self.frame.new_label('Brightness', 'kairon-10')
+        self.frame.paste(1, 0, label, cv=self.cv)
+        label = self.frame.new_label('Wi-Fi connection', 'kairon-10')
+        self.frame.paste(1, 11, label, cv=self.cv)
+        label = self.frame.new_label('Lifelines', 'kairon-10')
+        self.frame.paste(1, 22, label, cv=self.cv)
+        self.reader.reset()
+
+    def step(self):
+        self.reader.step(self.app.receive)
+        self.dial_reader.step(self.app.receive)
+        # TODO: Currently every mode's step() method must call self.frame.send()
+        # in order for sdl_frame to detect events; fix this leaky abstraction.
         self.frame.send()
 
-    def password_start(self):
-        self.frame.clear()
-        label = self.frame.new_label('Wi-Fi password:', 'kairon-10')
-        self.frame.paste(1, 0, label, cv=self.menu_cv)
-        self.state = 'PASSWORD'
 
+class PasswordMode(Mode):
+    def __init__(self, app, button_map, dial_map):
+        super().__init__(app)
+        self.cv = self.frame.pack(0x80, 0x80, 0x80)
+        self.cursor_cv = self.frame.pack(0x00, 0xff, 0x00)
+        self.reader = ButtonReader({
+            button_map['UP']: {
+                Press.SHORT: 'NEXT_CHAR',
+                Press.REPEAT: 'NEXT_CHAR',
+            },
+            button_map['DOWN']: {
+                Press.SHORT: 'ENTER_CHAR',
+                Press.LONG: 'CLOCK_MODE',
+            },
+            button_map['ENTER']: {
+                Press.SHORT: 'ENTER_CHAR',
+                Press.LONG: 'CLOCK_MODE',
+            }
+        })
+        self.dial_reader = DialReader('SELECTOR', dial_map['SELECTOR'], 1)
         self.charset = (
             ' aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ' +
             '0123456789' +
@@ -184,70 +260,49 @@ class Clock:
             '/\\()[]<>{}~'
         )
         self.char_index = 0
-        self.char = self.charset[0]
-        self.text = ''
-        self.password_update_text()
 
-    def password_step(self):
-        self.password_selector_reader.step(self.receive)
-        self.password_reader.step(self.receive)
+    def start(self):
+        self.reader.reset()
+        self.frame.clear()
+
+        label = self.frame.new_label('Wi-Fi password:', 'kairon-10')
+        self.frame.paste(1, 0, label, cv=self.cv)
+
+        self.text = ''
+        self.draw_text()
+
+    def step(self):
+        self.reader.step(self.app.receive)
+        self.dial_reader.step(self.app.receive)
+        # TODO: Currently every mode's step() method must call self.frame.send()
+        # in order for sdl_frame to detect events; fix this leaky abstraction.
         self.frame.send()
 
-    def password_update_char(self):
+    def draw_char(self):
         self.char = self.charset[self.char_index]
         char_label = self.frame.new_label(self.char, 'kairon-10')
         x = 1 + self.text_label.w
-        self.frame.paste(x, 16, char_label, cv=self.edit_cv)
-        self.frame.fill(x, 26, char_label.w - 1, 1, self.menu_cv)
+        self.frame.paste(x, 16, char_label, cv=self.cursor_cv)
+        self.frame.fill(x, 26, char_label.w - 1, 1, self.cv)
         self.frame.clear(x + char_label.w - 1, 16, 10, 12)
 
-    def password_update_text(self):
+    def draw_text(self):
         self.text_label = self.frame.new_label(self.text, 'kairon-10')
-        self.frame.paste(1, 16, self.text_label, cv=self.menu_cv)
+        self.frame.paste(1, 16, self.text_label, cv=self.cv)
         self.frame.clear(1, 26, self.text_label.w, 1)
-        self.password_update_char()
+        self.draw_char()
 
     def receive(self, command, arg=None):
-        if arg is not None:
-            print(f'[{command}: {arg}]')
-        else:
-            print(f'[{command}]')
-        if command == 'NEXT_LANGUAGE':
-            self.frame.clear()
-            self.lang_index = (self.lang_index + 1) % len(self.langs)
-        if command == 'TOGGLE_CASE':
-            self.frame.clear()
-            self.force_upper = not self.force_upper
-        if command == 'NEXT_LIFELINE':
-            self.incr_lifeline(1)
-        if command == 'PREV_LIFELINE':
-            self.incr_lifeline(-1)
-        if command == 'CLOCK':
-            self.clock_start()
-        if command == 'MENU':
-            self.menu_start()
-        if command == 'PASSWORD':
-            self.password_start()
         if command == 'NEXT_CHAR':
             self.char_index = (self.char_index + 1) % len(self.charset)
-            self.password_update_char()
-        if command == 'ENTER_CHAR':
-            self.text += self.char
-            self.password_update_text()
-        if command == 'BRIGHTNESS':
-            delta, value = arg
-            self.frame.set_brightness(value)
-        if command == 'PASSWORD_SELECTOR':
+            self.draw_char()
+        if command == 'SELECTOR':
             delta, value = arg
             self.char_index = (self.char_index + len(self.charset) + delta) % len(self.charset)
-            self.password_update_char()
-
-    def incr_lifeline(self, delta):
-        self.frame.clear()
-        self.lifeline_index = (
-            self.lifeline_index + len(self.lifeline_modules) + delta
-        ) % len(self.lifeline_modules)
-        self.frame.clear()
+            self.draw_char()
+        if command == 'ENTER_CHAR':
+            self.text += self.char
+            self.draw_text()
 
 
 debug.mem('clock8')
@@ -256,6 +311,7 @@ debug.mem('clock8')
 def run(fs, network, frame, button_map, dial_map):
     cctime.enable_rtc()
     data = ccapi.load_file('cache/climateclock.json')
-    clock = Clock(fs, network, data, frame, button_map, dial_map)
+    app = App(fs, network, data, frame, button_map, dial_map)
+    app.start()
     while True:
-        clock.step()
+        app.step()
