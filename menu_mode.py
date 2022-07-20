f340f1aa9eec89ad7fdcefaa4424de1dd331005c
Ka-Ping Yee <zestyping@gmail.com>
2022-07-19 19:09:05 -0700

    Simplify menu tree navigation logic.

--- menu_mode.py
+++ menu_mode.py
@@ -32,60 +32,70 @@ class MenuMode(Mode):
         self.frame.clear()
         wifi_ssid = self.app.prefs.get('wifi_ssid')
         updater = self.app.updater
-        index_fetched = updater.index_fetched and updater.index_fetched.isoformat()
+        index_updated = updater.index_updated or 'None'
+        index_fetched = (updater.index_fetched and
+            updater.index_fetched.isoformat() or 'Not yet')
         software_version = sys.path[0]
-        firmware_version = self.app.network.get_firmware_version()
-        hardware_address = self.app.network.get_hardware_address()
+        esp_firmware_version = self.app.network.get_firmware_version()
+        esp_hardware_address = self.app.network.get_hardware_address()
         sec = self.app.prefs.get('auto_cycling_sec')
         auto_cycling = sec and f'{sec} seconds' or 'Off'
-        self.tree = ('Settings', None, [
-            ('Wi-Fi setup', None, [
-                ('Network: ' + wifi_ssid, ('WIFI_SSID_MODE', None), []),
-                ('Password', ('WIFI_PASSWORD_MODE', None), []),
-                ('Back', ('BACK', None), [])
+
+        # Each node has the form (title, value, command, arg, children).
+        self.tree = ('Settings', None, None, None, [
+            ('Wi-Fi setup', None, None, None, [
+                ('Network', wifi_ssid, 'WIFI_SSID_MODE', None, []),
+                ('Password', None, 'WIFI_PASSWORD_MODE', None, []),
+                ('Back', None, 'BACK', None, [])
             ]),
-            (f'Auto cycling: {auto_cycling}', None, [
-                ('Off', ('SET_CYCLING', 0), []),
-                ('15 seconds', ('SET_CYCLING', 15), []),
-                ('60 seconds', ('SET_CYCLING', 60), []),
-                ('Back', ('BACK', None), [])
+            (f'Auto cycling', auto_cycling, None, None, [
+                ('Off', None, 'SET_CYCLING', 0, []),
+                ('15 seconds', None, 'SET_CYCLING', 15, []),
+                ('60 seconds', None, 'SET_CYCLING', 60, []),
+                ('Back', None, 'BACK', None, [])
             ]),
-            ('System info', None, [
-                (updater.index_name or 'Climate Clock', None, []),
-                (f'Version: {software_version}', None, []),
-                (f'Time: {cctime.get_datetime().isoformat()}', None, []),
-                (f'Index version: {updater.index_updated}', None, []),
-                (f'Index fetched: {updater.index_fetched}', None, []),
-                (f'ESP firmware: {firmware_version}', None, []),
-                (f'MAC ID: {hardware_address}', None, []),
-                ('Back', ('BACK', None), [])
+            ('System info', None, None, None, [
+                (updater.index_name or 'Climate Clock', None, None, None, []),
+                (f'Version', software_version, None, None, []),
+                (f'Time', cctime.get_datetime().isoformat(), None, None, []),
+                (f'Index version', index_updated, None, None, []),
+                (f'Index fetched', index_fetched, None, None, []),
+                (f'ESP firmware', esp_firmware_version, None, None, []),
+                (f'MAC ID', esp_hardware_address, None, None, []),
+                ('Back', None, 'BACK', None, [])
             ]),
-            ('Exit', ('CLOCK_MODE', None), [])
+            ('Exit', None, 'CLOCK_MODE', None, [])
         ])
+        self.node = None
         self.crumbs = []
-        self.top = self.index = self.offset = 0
         self.proceed(self.tree)
 
     def proceed(self, node):
-        title, command_arg, children = node
-        if command_arg:
-            self.app.receive(*command_arg)
+        title, value, command, arg, children = node
+        if command:
+            self.app.receive(command, arg)
         else:
-            self.crumbs.append((node, self.top, self.index))
+            if self.node:
+                self.crumbs.append((self.node, self.top, self.index))
+            self.node = node
             self.top = self.index = self.offset = 0
             self.draw()
 
     def draw(self):
-        (title, command, children), _, _ = self.crumbs[-1]
+        title, value, command, arg, children = self.node
         self.frame.clear()
+        if value and not children:
+            title += ': ' + value
         label = self.frame.new_label(title, 'kairon-10')
         self.frame.paste(1 - self.offset, 0, label, cv=self.cv)
         y = 0
         for index in range(self.top, self.top + 3):
             if index >= len(children):
                 break
-            child = children[index]
-            label = self.frame.new_label(child[0], 'kairon-10')
+            child_title, child_value, _, _, _ = children[index]
+            if child_value:
+                child_title += ': ' + child_value
+            label = self.frame.new_label(child_title, 'kairon-10')
             self.frame.paste(64, y, label, cv=self.cv)
             if index == self.index:
                 self.frame.paste(58, y, self.cursor_label, cv=self.cursor_cv)
@@ -110,18 +120,18 @@ class MenuMode(Mode):
             self.app.prefs.set('auto_cycling_sec', arg)
             self.app.receive('MENU_MODE')  # reformat the menu strings
         if command == 'PROCEED':
-            (title, command_arg, children), _, _ = self.crumbs[-1]
+            title, value, command, arg, children = self.node
             if children:
                 self.proceed(children[self.index])
             else:
                 command = 'BACK'
         if command == 'BACK':
-            _, self.top, self.index = self.crumbs.pop()
+            self.node, self.top, self.index = self.crumbs.pop()
             self.offset = 0
             self.draw()
 
     def move_cursor(self, delta):
-        (title, command, children), _, _ = self.crumbs[-1]
+        title, value, command, arg, children = self.node
         if children:
             self.index = max(0, min(len(children) - 1, self.index + delta))
             self.top = max(self.index - 2, min(self.index, self.top))
