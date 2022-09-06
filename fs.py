dbbf06534d85274ddeea838f9fa46bbe4cf84a9d
Ka-Ping Yee <zestyping@gmail.com>
2022-09-06 04:55:36 -0700

    Fix missing mode in fs.write.

--- fs.py
+++ fs.py
@@ -15,7 +15,7 @@ def open(relpath, mode='rb'):
 
 
 def write(relpath, content, mode='wb'):
-    with open(relpath, 'wb') as file:
+    with open(relpath, mode) as file:
         file.write(content)
 
 
