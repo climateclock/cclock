96d3bddca1aeec6f8713af5a017b0386718bc698
Ka-Ping Yee <zestyping@gmail.com>
2022-10-15 13:11:46 -0700

    Set default update failure delay to 60 seconds, success delay to 60 minutes.

--- updater.py
+++ updater.py
@@ -12,8 +12,8 @@ import utils
 # All durations are measured in milliseconds.
 INITIAL_DELAY = prefs.get_int('updater_initial_delay', 1000)
 WIFI_DELAY = prefs.get_int('updater_wifi_delay', 1000)
-FAILURE_DELAY = prefs.get_int('updater_failure_delay', 30 * 1000)
-SUCCESS_DELAY = prefs.get_int('updater_success_delay', 30 * 60 * 1000)
+FAILURE_DELAY = prefs.get_int('updater_failure_delay', 60 * 1000)
+SUCCESS_DELAY = prefs.get_int('updater_success_delay', 60 * 60 * 1000)
 MIN_RESTART_UPTIME = prefs.get_int('min_restart_uptime', 60 * 60 * 1000)
 
 
